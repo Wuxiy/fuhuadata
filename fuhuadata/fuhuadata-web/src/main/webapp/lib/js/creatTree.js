f786314fdb574d18dcce537de5a18d19dfe25e86
Xiangyang Hu <Xiangyang Hu>
2017-02-24 11:15:55 +0800

    1234

---
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/creatTree.js
@@ -0,0 +1,38 @@
++ function ($) {
+	'use strict';
+	var menulist = {
+        "menulist": [
+             { "MID": "M001", "MName": "首页", "Url": "#", "menulist": "" },
+            { "MID": "M002", "MName": "车辆买卖", "Url": "#", "menulist":
+                 [
+                      { "MID": "M003", "MName": "新车", "Url": "#", "menulist":
+                         [
+                             { "MID": "M006", "MName": "奥迪", "Url": "#", "menulist": "" },
+                             { "MID": "M007", "MName": "别克", "Url": "#", "menulist": "" }
+                         ]
+                      },
+                      { "MID": "M004", "MName": "二手车", "Url": "#", "menulist": "" },
+                      { "MID": "M005", "MName": "改装车", "Url": "#", "menulist": "" }
+                 ]
+             },
+             { "MID": "M006", "MName": "宠物", "Url": "#", "menulist": "" }
+       ]
+     };
+	var treeData = menulist.menulist;
+	var $tree = $("#characterTree");
+	var treeRoot = $('<ul class="tree-root"></ul>');
+	$tree.html("");
+	creatTree(treeData,treeRoot);
+    $tree.append(treeRoot);
+    function creatTree(obj,parent) {
+        $.each(obj, function(n,item){
+	     	if(item.menulist.length>0) {
+                 var li = $("<li></li>");
+                 $(li).append('<span class="branch-node" data-toggle="collapse" data-target="#'+item.MID+'"></span><a href=""><span class="leaf"></span>'+item.MName+'</a>').append('<ul id="'+item.MID+'" class="tree-branch collapse in"></ul>').appendTo(parent);
+                 creatTree(item.menulist, $(li).children("ul.tree-branch"));
+	     	}else{
+                $("<li></li>").append('<span class="branch"></span><a href=""><span class="leaf"></span>'+item.MName+'</a>').appendTo(parent);
+	     	}
+        });
+     };
+}(jQuery);
