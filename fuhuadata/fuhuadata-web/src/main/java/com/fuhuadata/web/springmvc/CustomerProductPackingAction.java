aea45b7b5dab356129809f0c74ed5db46730f6dc
wuxiy <18224472596@163.com>
2017-03-09 20:04:22 +0800

    123

--- fuhuadata/fuhuadata-web/src/main/java/com/fuhuadata/web/springmvc/CustomerProductPackingAction.java
+++ fuhuadata/fuhuadata-web/src/main/java/com/fuhuadata/web/springmvc/CustomerProductPackingAction.java
@@ -1,23 +1,22 @@
 package com.fuhuadata.web.springmvc;
 
 
-<<<<<<< HEAD
-=======
-import com.fuhuadata.domain.CustomerProductInfo;
-import com.fuhuadata.domain.ExhibitionInfo;
-import com.fuhuadata.domain.query.ExhibitionInfoQuery;
+
 import com.fuhuadata.domain.query.Result;
 import com.fuhuadata.domain.query.ResultPojo;
 import com.fuhuadata.service.CustomerProductInfoService;
-import com.fuhuadata.service.ExhibitionInfoService;
 import com.fuhuadata.vo.CustomerProductPackagingArchives;
-import com.fuhuadata.web.util.DateUtil;
->>>>>>> dfcd8437d233130064b3dd4cc16a763331d9205c
 import com.fuhuadata.web.util.SystemLogAnnotation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 客户产品包装要求action
@@ -28,7 +27,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/customerProductPacking/*")
 public class CustomerProductPackingAction {
 
-    private final static Log log=LogFactory.getLog(CustomerProductPackingAction.class);
+    private final static Log log= LogFactory.getLog(CustomerProductPackingAction.class);
 
     @Resource
     private CustomerProductInfoService customerProductInfoService;
