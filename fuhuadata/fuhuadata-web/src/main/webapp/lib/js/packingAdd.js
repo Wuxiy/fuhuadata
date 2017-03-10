77d65b2684f3f54b419c610f063a84f5da0f7f56
Loyun <Loyun>
2017-03-10 13:47:23 +0800

    0310

--- fuhuadata/fuhuadata-web/src/main/webapp/lib/js/packingAdd.js
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/packingAdd.js
@@ -11,12 +11,14 @@
     var names = document.getElementById('packName');
     names.value = name;
 
+    var img = $("input[name='file']");
+    var imgnames =$("input[name='imagname']");
 
     var checkboxarr = '';
-    var imgpaths;
-    var imgnames;
-    var imgpath = {imgpaths:imgnames};
+    var obj1 = {},obj2 = {},obj3 = {};
+    var arr = [];
     var ids = new Array();
+
 
     //根据包材类型获取关联包材的展示
     if(bid != 1){
@@ -27,7 +29,45 @@
 
 //图片上传
 
-$("input[name='file']").fileinput({
+/*function imag(){
+    img.each(function(){
+        $("input[name='file']").fileinput({
+            language: 'zh', //设置语言
+            uploadUrl: '/upload/uploadFile', // you must set a valid URL here else you will get an error
+            allowedFileExtensions : ['jpg', 'png','gif'],
+            overwriteInitial: false,
+            maxFileSize: 1000,
+            maxFilesNum: 10,
+            maxImageWidth: 100,//图片的最大宽度
+            maxImageHeight: 100,//图片的最大高度
+            //allowedFileTypes: ['image', 'video', 'flash'],
+            enctype: 'multipart/form-data',
+            slugCallback: function(filename) {
+                console.log(filename);
+
+                return filename.replace('(', '_').replace(']', '_');
+            }
+        }).on("filebatchselected", function(event, file) {
+            console.log('233');
+
+        }).on("fileuploaded", function(event, data) {
+
+            if(data.response)
+            {
+                alert('处理成功');
+            }
+            obj.path = data.response.data;
+            imgnames.each(function(){
+               obj.name = $(this).val();
+            });
+            arr.push(obj);
+            console.log(arr);
+        });
+    })
+
+}*/
+
+$("input[id='file-1']").fileinput({
     language: 'zh', //设置语言
     uploadUrl: '/upload/uploadFile', // you must set a valid URL here else you will get an error
     allowedFileExtensions : ['jpg', 'png','gif'],
@@ -39,19 +79,86 @@ $("input[name='file']").fileinput({
     //allowedFileTypes: ['image', 'video', 'flash'],
     enctype: 'multipart/form-data',
     slugCallback: function(filename) {
+        console.log(filename);
+        $('#imagname1').val(filename);
         return filename.replace('(', '_').replace(']', '_');
     }
 }).on("filebatchselected", function(event, file) {
     console.log('233');
 
 }).on("fileuploaded", function(event, data) {
-        console.log('233');
+
         if(data.response)
         {
             alert('处理成功');
         }
-        imgpaths.push(data.response.data);
+        obj1.path = data.response.data;
+        obj1.name = $('#imagname1').val();
+        arr.push(obj1);
+        console.log(arr);
     });
+
+$("input[id='file-2']").fileinput({
+    language: 'zh', //设置语言
+    uploadUrl: '/upload/uploadFile', // you must set a valid URL here else you will get an error
+    allowedFileExtensions : ['jpg', 'png','gif'],
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    maxFilesNum: 10,
+    maxImageWidth: 100,//图片的最大宽度
+    maxImageHeight: 100,//图片的最大高度
+    //allowedFileTypes: ['image', 'video', 'flash'],
+    enctype: 'multipart/form-data',
+    slugCallback: function(filename) {
+        console.log(filename);
+        $('#imagname2').val(filename);
+        return filename.replace('(', '_').replace(']', '_');
+    }
+}).on("filebatchselected", function(event, file) {
+    console.log('233');
+
+}).on("fileuploaded", function(event, data) {
+
+    if(data.response)
+    {
+        alert('处理成功');
+    }
+    obj2.path = data.response.data;
+    obj2.name = $('#imagname2').val();
+    arr.push(obj2);
+    console.log(arr);
+});
+
+$("input[id='file-3']").fileinput({
+    language: 'zh', //设置语言
+    uploadUrl: '/upload/uploadFile', // you must set a valid URL here else you will get an error
+    allowedFileExtensions : ['jpg', 'png','gif'],
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    maxFilesNum: 10,
+    maxImageWidth: 100,//图片的最大宽度
+    maxImageHeight: 100,//图片的最大高度
+    //allowedFileTypes: ['image', 'video', 'flash'],
+    enctype: 'multipart/form-data',
+    slugCallback: function(filename) {
+        console.log(filename);
+        $('#imagname3').val(filename);
+        return filename.replace('(', '_').replace(']', '_');
+    }
+}).on("filebatchselected", function(event, file) {
+    console.log('233');
+
+}).on("fileuploaded", function(event, data) {
+
+    if(data.response)
+    {
+        alert('处理成功');
+    }
+    obj3.path = data.response.data;
+    obj3.name = $('#imagname3').val();
+    arr.push(obj3);
+    console.log(arr);
+});
 
 //适用产品类型checkbox
 $("input[name='check']").each(function(index,element){
@@ -81,7 +188,7 @@ $('.packingAdd').on('click',function(){
         "priceEndDate": jQuery('#priceEndDate').val(),
         "status": jQuery('#status').val(),
         "suitableType": checkboxarr,
-        "imgpath":imgpath,
+        "image":arr,
         "ids":ids,
         "bremarks": jQuery('#bremarks').val(),
     }
@@ -116,6 +223,11 @@ $('#modal_checkAll').on('click',function(){
             });
         }
     });
+})
+
+
+$('.update').hover(function(){
+
 })
 
 
