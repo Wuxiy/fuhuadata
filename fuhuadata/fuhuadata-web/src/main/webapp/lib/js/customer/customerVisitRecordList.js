7ef3f8585649b6b12ff33b82ae696416d273f469
Loyun <Loyun>
2017-03-22 15:05:30 +0800

    0322

--- fuhuadata/fuhuadata-web/src/main/webapp/lib/js/customer/customerVisitRecordList.js
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/customer/customerVisitRecordList.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
 })
 
 function visitList(result) {
+    var visitList = document.getElementById('visitList');
     var ResultData = result;
     console.log(ResultData);
+
+    for(var i=0;i<ResultData.length;i++){
+        visitList.innerHTML += '<tr>'+
+                                '<td>'+ResultData.startTime+'</td>'+
+                                '<td>'+ResultData.endTime+'</td>'+
+                                '<td>'+ResultData.activityType+'</td>'+
+                                '<td>'+ResultData.activityAddress+'</td>'+
+                                '<td>'+ResultData.activityExpens+'</td>'+
+                                '<td>'+ResultData.name+'</td>'+
+                                '<td>'+ResultData.activityGift+'</td>'+
+                                '<td>'+ResultData.activitySummary+'</td>'+
+                                '</tr>';
+    }
 }
