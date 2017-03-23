43f450e4dcebc8ecc73c02cd49f5342b22d27c87
Loyun <Loyun>
2017-03-23 15:33:38 +0800

    0323

---
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/customer/customerAnotherNamer.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Loyun on 2017/3/23.
+ */
+
+$(document).ready(function(){
+    getData(basePath + '/customerSubcompanyInfo/getCustomerSubcompanyInfosByCustomerId','POST',GetRequest(),anotherNamerList);
+})
+
+//渲染List
+function anotherNamerList(result) {
+    var ResultData = result;
+    var AnotherNameList = document.getElementById('anotherNameList');
+    console.log(ResultData);
+    AnotherNameList.innerHTML = '';
+
+    for(var i=0;i<ResultData.length;i++){
+        AnotherNameList.innerHTML += '<tr>'+
+                                        '<td>'+ResultData[i].customerSubId+'</td>'+
+                                        '<td><a class="otherNameinfo" data_url="'+basePath+'/customerSubcompanyInfo/getCustomerSubcompanyInfoById?id='+ResultData[i].customerSubId+'">'+ResultData[i].fullName+'</a></td>'+
+                                        '<td>'+ResultData[i].shortName+'</td>'+
+                                        '<td>'+ResultData[i].property+'</td>'+
+                                        '<td>'+ResultData[i].zhongxinbaoNumber+'</td>'+
+                                        '<td>'+ResultData[i].zhongxinbaoLevel+'</td>'+
+                                        '<td>'+ResultData[i].customerSubRemarks+'</td>'+
+                                        '</tr>';
+    }
+}
+
+$(document).on('click','.otherNameinfo',function(){
+    var url = $(this).attr('data_url');
+
+    jQuery.ajax({
+        type:'POST',
+        url:url,
+        success:function(result){
+            var ResultData = result;
+            var arr = ResultData.property.split(',');
+            $.each(arr,function(index,suitname){
+                $("input[name='check']").each(function(){
+                        if($(this).val() == suitname){
+                            $(this).attr('checked',true);
+                            if(suitname == 4){
+                                $('#propertyRemarks').val(ResultData.propertyRemarks);
+                            }
+                        }
+                })
+            })
+            $('#packName').val(ResultData.fullName);
+            $('#spec').val(ResultData.shortName);
+            $('#size').val(ResultData.zhongxinbaoNumber);
+            $('#quality').val(ResultData.zhongxinbaoLevel);
+            $('#qualityIndex').val(ResultData.customerSubRemarks);
+
+            $('#addFieldinfo').modal('show');
+        }
+    })
+})
