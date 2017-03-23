d0f14a98df6003689ef4bfa2f4d8ed06d7a90221
hxy <huxiangyang2333@gmail.com>
2017-03-23 21:45:29 +0800

    3236

--- fuhuadata/fuhuadata-web/src/main/webapp/lib/js/customer/common.js
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/customer/common.js
@@ -6,7 +6,7 @@
  * 给副导航href绑定参数
  */
 var urlPar = location.search;
-$('#deputyNav').find('a').attr('href',function(i,val){
+$('#deputyNav,#asideNav').find('a').attr('href',function(i,val){
     // console.log(val+urlPar);
     return val+urlPar;
 });
