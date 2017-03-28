6e7adc7fad725164178ead0a6a6a9975babf8f4f
Loyun <Loyun>
2017-03-28 14:05:40 +0800

    0328

---
+++ fuhuadata/fuhuadata-web/src/main/webapp/lib/js/saleprocess/businessOpportunityInfo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Loyun on 2017/3/28.
+ */
+
+$(document).ready(function(){
+    getData(basePath + '','POST',GetRequest(),businessopportunityinfo);
+})
+
+function businessopportunityinfo(result) {
+
+}
