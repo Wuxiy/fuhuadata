a435872dc118cf7dd87bfdbe20de9e6048756b45
hexingfu <hexingfu@Think>
2017-04-11 11:26:26 +0800

    ''

---
+++ fuhuadata/fuhuadata-domain/src/main/java/com/fuhuadata/vo/PriceDetail.java
@@ -0,0 +1,21 @@
+package com.fuhuadata.vo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by hexingfu on 2017/4/10.
+ */
+public class PriceDetail implements Serializable {
+    //费用名称
+    private String costName;
+    //单位
+    private String unit;
+    //单价
+    private BigDecimal price;
+    //数量
+    private BigDecimal num;
+    //金额
+    private BigDecimal money;
+
+}
