b69de452ab7d5ade52055d207fff71e8ded1fcc4
wuxiy <18224472596@163.com>
2017-04-20 17:59:52 +0800

    order

--- fuhuadata/fuhuadata-web/src/main/java/com/fuhuadata/web/springmvc/BusinessOrderAction.java
+++ fuhuadata/fuhuadata-web/src/main/java/com/fuhuadata/web/springmvc/BusinessOrderAction.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import sun.rmi.runtime.Log;
 
 import java.util.List;
 
@@ -158,8 +157,9 @@ public class BusinessOrderAction {
      *  报价转化订单
      * @param
      */
-    @RequestMapping("/updateOrderProducts")
+    @RequestMapping(value = "/updateOrderProducts",method = RequestMethod.POST)
     @SystemLogAnnotation(module = "salesStatistics",methods = "updateOrderProducts")
+    @ResponseBody
     public ResultPojo updateOrderProducts(@RequestBody BusinessOrderDO businessOrderDO){
         Result result = new Result();
         try{
