1aced09e07a3a9f9d1627ef976a63e3839ec97cc
wangjie <wangjie@fuhua.com>
2017-11-10 11:12:35 +0800

    加工厂、货代添加权限控制

---
+++ fuhuadata/fuhuadata-dao/src/main/resources/db/migration/V8__Permission_of_supplier.sql
@@ -0,0 +1,8 @@
+/* 加工厂添加按钮 */
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('add', 14, '新增');
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('edit', 14, '编辑');
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('delete', 14, '删除');
+/* 货代物流添加按钮 */
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('add', 15, '新增');
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('edit', 15, '编辑');
+INSERT INTO p_button(permission, menu_id, button_name) VALUES ('delete', 15, '删除');
